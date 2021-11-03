9678ee2a63b7c26151521bd4160f397c088e7edc
GitAPhan <itsandrewphan@gmail.com>
2021-11-02 21:25:44 -0600

    added in the catch[D[D[D[D[Delse catch

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ if (userAge >= 18 && userSubscribe === true) {
     console.log('the user is younger than 18 and is subscribed')
 } else if (userAge >= 18 && userSubscribe === false) {
     console.log('the user is 18 or older and not subscribed')
-} else {
+} else if (userAge < 18 && userSubscribe === true) {
     console.log('the user is younger than 18 and not subscribed')
+} else {
+    console.log('an error has occured!')
 }
