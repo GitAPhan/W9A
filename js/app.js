de51e769e1d8d425b1bc723676e797e4e29dc339
GitAPhan <itsandrewphan@gmail.com>
2021-11-02 21:28:44 -0600

    error in the 4th condition

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ if (userAge >= 18 && userSubscribe === true) {
     console.log('the user is younger than 18 and is subscribed')
 } else if (userAge >= 18 && userSubscribe === false) {
     console.log('the user is 18 or older and not subscribed')
-} else if (userAge < 18 && userSubscribe === true) {
+} else if (userAge < 18 && userSubscribe === false) {
     console.log('the user is younger than 18 and not subscribed')
 } else {
     console.log('an error has occured!')
