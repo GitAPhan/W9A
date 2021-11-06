1c77f9df660f9511fc79ec5150f62398c758e3c8
GitAPhan <itsandrewphan@gmail.com>
2021-11-06 09:02:27 -0600

    Finshed W9B[D[D[Dassignment W9B, added a loop(conditional) and converted the variables into arrays

--- js/app.js
+++ js/app.js
@@ -15,3 +15,19 @@ if (userAge >= 18 && userSubscribe === true) {
 
 userAge = ["19", "23", "42", "15", "10", "27"];
 userSubscribe = [true, false, true, true, false, false];
+
+var user_length = userAge.length;
+
+for(var count = 0; count < user_length; count++) {
+    if (userAge[count] >= 18 && userSubscribe[count] === true) {
+        console.log('the user is 18 or older and is subscribed')
+    } else if (userAge[count] < 18 && userSubscribe[count] === true) {
+        console.log('the user is younger than 18 and is subscribed')
+    } else if (userAge[count] >= 18 && userSubscribe[count] === false) {
+        console.log('the user is 18 or older and not subscribed')
+    } else if (userAge[count] < 18 && userSubscribe[count] === false) {
+        console.log('the user is younger than 18 and not subscribed')
+    } else {
+        console.log('an error has occured!')
+    };
+}
